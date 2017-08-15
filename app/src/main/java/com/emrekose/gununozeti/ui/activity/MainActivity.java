197a4e69b9bd29b1036c434f8a3f6e2a3aac3284
Emre Köse <e.kose26@gmail.com>
2017-08-15 16:08:10 +0300

    main activity and recyclerview row design

--- app/src/main/java/com/emrekose/gununozeti/ui/activity/MainActivity.java
+++ app/src/main/java/com/emrekose/gununozeti/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.emrekose.gununozeti;
+package com.emrekose.gununozeti.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.emrekose.gununozeti.R;
 
 public class MainActivity extends AppCompatActivity {
 
