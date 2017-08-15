a834772faf514e064f65c71746b67f7db3f52e24
Emre Köse <e.kose26@gmail.com>
2017-08-15 18:57:41 +0300

    Go to past date with datePickerDialog

--- app/src/main/java/com/emrekose/gununozeti/ui/activity/MainActivity.java
+++ app/src/main/java/com/emrekose/gununozeti/ui/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package com.emrekose.gununozeti.ui.activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -18,6 +20,7 @@ import com.emrekose.gununozeti.mvp.view.SummaryView;
 import com.emrekose.gununozeti.ui.adapter.SummaryRecyclerAdapter;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -26,7 +29,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import dagger.android.AndroidInjection;
-import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements SummaryView {
 
@@ -39,10 +41,15 @@ public class MainActivity extends AppCompatActivity implements SummaryView {
     @BindView(R.id.summaryDate)
     TextView summaryDate;
 
+    @BindView(R.id.fabChange)
+    FloatingActionButton fab;
+
     @Inject
     SummaryPresenter presenter;
 
     SummaryRecyclerAdapter adapter;
+
+    private int mYear, mMonth, mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +59,11 @@ public class MainActivity extends AppCompatActivity implements SummaryView {
         ButterKnife.bind(this);
 
         presenter.loadSummaries("2017-08-14");
+
+
+        fab.setOnClickListener(v -> {
+            getDatePickerDialog();
+        });
     }
 
     @Override
@@ -75,17 +87,31 @@ public class MainActivity extends AppCompatActivity implements SummaryView {
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
-
-        for (Content content: response.getSummaries().values()){
-            Timber.e(content.getTitle());
-            Timber.e(content.getUrl());
-        }
     }
 
     @OnClick(R.id.avatarLayout)
-    public void avatarOnClick(){
+    public void avatarOnClick() {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse("http://www.twitter.com/HasanYasar"));
         startActivity(i);
+    }
+
+    private void getDatePickerDialog() {
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (datePicker, year, month, day) -> {
+            String responseDate = month < 10 ? year + "-" + "0" + (month + 1) + "-" + day : year + "-" + "0" + (month + 1) + "-" + day;
+            presenter.loadSummaries(responseDate);
+
+        }, mYear, mMonth, mDay);
+
+        // disable future days
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+        datePickerDialog.show();
     }
 }
